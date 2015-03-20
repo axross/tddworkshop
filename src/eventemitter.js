fe103b567b83989942d9a65d7f10cdfb0890e23d
安田裕介 <yyusuke@trifort.jp>
2015-03-20 19:56:28 +0900

    add EventEmitter#off()

--- src/eventemitter.js
+++ src/eventemitter.js
@@ -12,8 +12,19 @@ EventEmitter.prototype = {
         this._events[event].push(callback);
         return this;
     },
+    off: function(event, callback) {
+        if (!this._events[event]) {
+            this._events[event] = [];
+        }
+        var index = this._events[event].indexOf(callback);
+        if (index !== -1) {
+            this._events[event].splice(index, 1);
+        }
+        return this;
+    },
     emit: function(event, payload) {
-        this._events[event].forEach(function(callback) {
+        var events = this._events[event] || [];
+        events.forEach(function(callback) {
             callback.call(this, payload);
         }.bind(this));
         return this;
--- src/eventemitter.mocha.js
+++ src/eventemitter.mocha.js
@@ -6,9 +6,23 @@ var EventEmitter = require('./eventemitter');
 describe('EventEmitter', function() {
     it('event.emit', function() {
         var event = new EventEmitter();
+        event.emit('test', {test: 'Hello'});
         event.on('test', function(e) {
             assert.deepEqual(e, {test: 'Hello'});
         });
+        event.emit('test', {test: 'Hello'});
+    });
+
+    it('event.off', function() {
+        var event = new EventEmitter();
+
+        var fail = function(e) {
+            assert.fail('fail is called', 'fail is not called', 'event handler should not be called after removed');
+        };
+
+        event.emit('test', {test: 'Hello'});
+        event.on('test', fail);
+        event.off('test', fail);
         event.emit('test', {test: 'Hello'});
     });
 });
