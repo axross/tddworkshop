7c6f4d260baa9c2bed294911a0c01f634da070e7
安田裕介 <yyusuke@trifort.jp>
2015-03-21 18:53:14 +0900

    refine test comment

--- src/extend.mocha.js
+++ src/extend.mocha.js
@@ -4,13 +4,19 @@ var assert = require('power-assert');
 var extend = require('./extend');
 
 describe('extend', function() {
-    it('extend(a,b)', function() {
+    it('extend(a,b)はオブジェクトaにオブジェクトbのプロパティと値を追加し、合成する', function() {
         var a = {a: 1};
         var b = {b: 2};
         assert.deepEqual(extend(a,b), {a:1, b:2});
     });
 
-    it('extend({},obj)', function() {
+    it('extend(a,b)はオブジェクトaとオブジェクトbに同じ名前のキーがあった場合、オブジェクトbのプロパティを優先し、上書きする', function() {
+        var a = {a: 1, b: 1};
+        var b = {b: 2};
+        assert.deepEqual(extend(a,b), {a:1, b:2});
+    });
+
+    it('extend({},obj)はobjのコピーをつくる', function() {
         var obj = {a: 1, b: 2, c: 3};
         assert.deepEqual(extend({},obj), {a: 1, b: 2, c: 3});
         assert(extend({},obj) !== obj);
