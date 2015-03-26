d6c5825dadc6cf7d7e982634b7d9e37df8288d8f
安田裕介 <yyusuke@trifort.jp>
2015-03-26 15:16:47 +0900

    refine test description

--- src/vdom/attribute.karma.js
+++ src/vdom/attribute.karma.js
@@ -3,7 +3,7 @@
 var Attr = require('./attribute');
 
 describe('Attr', function() {
-    it('attr.serialize', function() {
+    it('attr.serialize()はコンストラクタで保持したオブジェクトをDOM属性形式の文字列でシリアライズする', function() {
         var attr = new Attr({class: 'test', id: 'test'});
         expect(attr.serialize()).toBe('class="test" id="test"');
     });
