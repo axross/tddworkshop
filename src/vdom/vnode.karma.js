5e42031cce133e23769f61d4004a8af29151f26d
安田裕介 <yyusuke@trifort.jp>
2015-03-26 15:22:02 +0900

    add test case

--- src/vdom/vnode.karma.js
+++ src/vdom/vnode.karma.js
@@ -1,21 +1,42 @@
 'use strict';
 
-var VNode = require('./vnode');
 var VText = require('./vtext');
+var VNode = require('./vnode');
+var Attr = require('./attribute');
 
 describe('VNode', function() {
-    it('vnode.render(name, attributes, text)', function() {
-        var div = VNode('div', {id: 'text'}, [ VText('中身') ]);
+    it('VNode(name, attributes, children)はnameを保持する', function() {
+        var div = new VNode('div', {id: 'text'}, [ new VText('中身') ]);
+        expect(div.name).toBe('div');
+    });
+
+    it('VNode(name, attributes, children)はattributesオブジェクトをAttrとして保持する', function() {
+        var div = new VNode('div', {id: 'text'}, [ new VText('中身') ]);
+        expect(div.attr).toEqual(new Attr({id: 'text'}));
+    });
+
+    it('VNode(name, attributes, children)はchildrenを保持する', function() {
+        var div = new VNode('div', {id: 'text'}, [ new VText('中身') ]);
+        expect(div.children).toEqual([ new VText('中身') ]);
+    });
+
+    it('VNode(name, attributes, children)はchildrenが渡されなければ[VText.empty()]で初期化する', function() {
+        var div = new VNode('div', {});
+        expect(div.children).toEqual([ VText.empty() ]);
+    });
+
+    it('VNode#render()はHTML文字列を返す', function() {
+        var div = new VNode('div', {id: 'text'}, [ VText('中身') ]);
         expect(div.render()).toBe('<div id="text" >中身</div>');
     });
 
-    it('vnode.render', function() {
-        var li = VNode('li', {class: 'item'});
-        var ul = VNode('ul', {}, [
+    it('VNode#render()はHTML文字列を返す', function() {
+        var li = new VNode('li', {class: 'item'});
+        var ul = new VNode('ul', {}, [
             li, li, li
         ]);
         expect(ul.render().replace(/[\n, \s]*/g, '')).toBe(
-            '<ul>\
+        '<ul>\
             <li class="item"></li>\
             <li class="item"></li>\
             <li class="item"></li>\
--- src/vdom/vnode.mocha.js
+++ src/vdom/vnode.mocha.js
@@ -3,16 +3,37 @@
 var assert = require('power-assert');
 var VText = require('./vtext');
 var VNode = require('./vnode');
+var Attr = require('./attribute');
 
 describe('VNode', function() {
-    it('vnode.render(name, attributes, text)', function() {
-        var div = VNode('div', {id: 'text'}, [ VText('中身') ]);
+    it('VNode(name, attributes, children)はnameを保持する', function() {
+        var div = new VNode('div', {id: 'text'}, [ new VText('中身') ]);
+        assert(div.name === 'div');
+    });
+
+    it('VNode(name, attributes, children)はattributesオブジェクトをAttrとして保持する', function() {
+        var div = new VNode('div', {id: 'text'}, [ new VText('中身') ]);
+        assert.deepEqual(div.attr, new Attr({id: 'text'}));
+    });
+
+    it('VNode(name, attributes, children)はchildrenを保持する', function() {
+        var div = new VNode('div', {id: 'text'}, [ new VText('中身') ]);
+        assert.deepEqual(div.children, [ new VText('中身') ]);
+    });
+
+    it('VNode(name, attributes, children)はchildrenが渡されなければ[VText.empty()]で初期化する', function() {
+        var div = new VNode('div', {});
+        assert.deepEqual(div.children, [ VText.empty() ]);
+    });
+
+    it('VNode#render()はHTML文字列を返す', function() {
+        var div = new VNode('div', {id: 'text'}, [ VText('中身') ]);
         assert(div.render() === '<div id="text" >中身</div>');
     });
 
-    it('vnode.render(name, attributes, children)', function() {
-        var li = VNode('li', {class: 'item'});
-        var ul = VNode('ul', {}, [
+    it('VNode#render()はHTML文字列を返す', function() {
+        var li = new VNode('li', {class: 'item'});
+        var ul = new VNode('ul', {}, [
             li, li, li
         ]);
         assert(ul.render().replace(/[\n, \s]*/g, '') ===
