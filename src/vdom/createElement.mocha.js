f0a0ad7faf1bd3aa1e8674f588cfe96768789b77
安田裕介 <yyusuke@trifort.jp>
2015-03-26 20:26:39 +0900

    add test cases

--- src/vdom/createElement.mocha.js
+++ src/vdom/createElement.mocha.js
@@ -6,12 +6,22 @@ var VText = require('./vtext');
 var createElement = require('./createElement');
 
 describe('createElement', function() {
-    it('createElement(name)', function() {
+    it('createElement(name)はnameを名前に持つVNodeを作るファクトリー関数をつくる', function() {
+        var div = createElement('div');
+        assert.deepEqual(div({id: 'hoge'}, [ VText('text') ]), VNode('div', {id: 'hoge'}, [ VText('text') ]));
+    });
+
+    it('createElement(name)で作った関数はchildrenが配列でない場合、配列にする', function() {
+        var div = createElement('div');
+        assert.deepEqual(div({id: 'hoge'}, VText('text')), VNode('div', {id: 'hoge'}, [ VText('text') ]));
+    });
+
+    it('createElement(name)で作った関数はchildrenが文字列の場合、VTextの配列にする', function() {
         var div = createElement('div');
         assert.deepEqual(div({id: 'hoge'}, 'text'), VNode('div', {id: 'hoge'}, [ VText('text') ]));
     });
 
-    it('createElement(name)', function() {
+    it('createElement(name)で作った関数はchildrenが指定されない場合、VText.empty()の配列にする', function() {
         var img = createElement('img');
         assert.deepEqual(img({src: 'hoge'}), VNode('img', {src: 'hoge'}, [ VText('') ]));
     });
