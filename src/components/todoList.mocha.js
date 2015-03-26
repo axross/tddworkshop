c945b6ef60e41fd212d6a7eac3224a0fd7a08369
安田裕介 <yyusuke@trifort.jp>
2015-03-26 20:59:32 +0900

    refine test comments

--- src/components/todoList.mocha.js
+++ src/components/todoList.mocha.js
@@ -8,7 +8,7 @@ var createElement = require('../vdom/createElement');
 describe('todoList', function() {
     var ul = createElement('ul');
 
-    it('todoList', function() {
+    it('todoListはtodoItemのリストである', function() {
         var list = [
             {id: 'test0', title: 'todoItem(item)', isPassed: true, tags: ['todoItem']},
             {id: 'test1', title: 'todoItem(item)', isPassed: false, tags: ['todoItem']}
