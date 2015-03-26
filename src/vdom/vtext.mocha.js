f02acb51be630f444fd16d2f89df33d6d45c8036
安田裕介 <yyusuke@trifort.jp>
2015-03-26 14:41:22 +0900

    refine test description

--- src/vdom/vtext.mocha.js
+++ src/vdom/vtext.mocha.js
@@ -4,12 +4,12 @@ var assert = require('power-assert');
 var VText = require('./vtext');
 
 describe('VText', function() {
-    it('text.render()', function() {
+    it('text.render()はコンストラクタで保持した文字列を返す', function() {
         var text = VText('text');
         assert(text.render() === 'text');
     });
 
-    it('VText.empty()', function() {
+    it('VText.empty()は空の文字列を保持するVTextである', function() {
         var empty = VText.empty();
         assert.deepEqual(empty, VText(''));
     });
